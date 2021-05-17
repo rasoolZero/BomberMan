1e56adeb0ed2291bf85ea08073c17b3ad8a3d7a4
rasoolZero <rasool.kamkar20@gmail.com>
2021-05-17 11:54:10 +0430

    basic SFML Window Added

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 4;
+    RenderWindow window(VideoMode::getDesktopMode(), "BomberMan", Style::Fullscreen,settings);
+    window.setVerticalSyncEnabled(true);
+    window.setFramerateLimit(30);
+
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(Color::White);
+        window.draw(quad,&texture);
+        window.display();
+    }
+
+    return 0;
+}
