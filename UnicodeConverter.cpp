05197bd4f502e678fcf3232239a8b198f4eb1b2d
saeed <saeedss2244@gmail.com>
2021-09-05 20:59:46 +0430

    fixed converter definition in UnicodeConverter.cpp

--- UnicodeConverter.cpp
+++ UnicodeConverter.cpp
@@ -1,5 +1,9 @@
 #include "UnicodeConverter.h"
+#ifdef _WIN32
 std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> UnicodeConverter::converter;
+#else //unix
+std::wstring_convert<std::codecvt_utf8<wchar_t>> UnicodeConverter::converter;
+#endif // _WIN32
 std::string UnicodeConverter::to_utf8(const std::wstring& wide_str)
 {
     return converter.to_bytes(wide_str);
